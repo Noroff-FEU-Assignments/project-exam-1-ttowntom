28a71b3f9c23228604439150b8bcc66b58e33f97
ttowntom <117744552+ttowntom@users.noreply.github.com>
2024-01-29 09:24:13 +0100

    added "load more" button

--- js/pages/blog.js
+++ js/pages/blog.js
@@ -4,11 +4,23 @@ import { getPosts } from "/js/api/index.js";
 const postsWrapper = document.querySelector("#posts");
 const featuredWrapper = document.querySelector("#featured");
 
+// Add event listener to load more button
+document.getElementById("btn-load-more").addEventListener("click", loadPosts);
+
+// Page tracking
+let currentPage = 1;
+const postsPerPage = 9;
+
 // Get all blog posts from server
 async function loadPosts() {
 	try {
-		let posts = await getPosts();
+		let posts = await getPosts(`per_page=${postsPerPage}&page=${currentPage}`);
 		renderPosts(posts);
+		// Remove load more button if there are no more posts to load
+		posts.length < postsPerPage
+			? document.getElementById("btn-load-more").remove()
+			: null;
+		currentPage++;
 	} catch (error) {
 		console.log("Error fetching posts:", error);
 	}
